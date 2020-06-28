2250f442471f7cb63d4784a5770a54fc192a298e
Parth Dodhia <58696335+pdudeia@users.noreply.github.com>
2020-06-28 14:25:03 +0530

    tuning using hyperopt

--- embeddings.py
+++ embeddings.py
@@ -6,16 +6,15 @@ import re
 import numpy as np
 from gensim.models import KeyedVectors
 from gensim.test.utils import common_texts, get_tmpfile, datapath
-from gensim.models import Word2Vec
 from gensim.models.callbacks import CallbackAny2Vec
 import multiprocessing
 import gensim
-import json
 import gc
-import progressbar
 import warnings
 from glob import glob
 from tqdm import tqdm
+import time
+from hyperopt import fmin, hp, tpe
 import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
@@ -51,10 +50,10 @@ class callback(CallbackAny2Vec):
     def on_epoch_end(self, model):
         loss = model.get_latest_training_loss()
         if self.epoch == 0:
-            print('Loss after epoch {}: {}'.format(self.epoch, loss))
+            # print('Loss after epoch {}: {}'.format(self.epoch, loss))
             self.losses.append(loss)
         else:
-            print('Loss after epoch {}: {}'.format(self.epoch, loss- self.loss_previous_step))
+            # print('Loss after epoch {}: {}'.format(self.epoch, loss- self.loss_previous_step))
             self.losses.append(loss-self.loss_previous_step)
         self.epoch += 1
         self.loss_previous_step = loss
@@ -81,8 +80,9 @@ def get_similar_words(text,model,look_up_list,refrence_word = None):
     list_words = [x for word in list_words for x in word]
     return unique(list_words)
 
-# model = KeyedVectors.load_word2vec_format('/home/31265/10K_Pandemic/glove.6B.300d.w2vformat.txt')
-#model = gensim.models.Word2Vec.load('/home/31265/10K_Pandemic/who_disease_outbreak.model')
+print('Loading pretrained model')
+model = KeyedVectors.load_word2vec_format('text_data/glove_pretrained/glove.6B.300d.w2vformat.txt')
+print('Finished loading pretrained model')
 
 def model_build(sentences, cobj,window=5,min_count=5,negative=5,sample=1e-3,epoch=1,alpha=0.025):
     cores = multiprocessing.cpu_count()
@@ -90,10 +90,10 @@ def model_build(sentences, cobj,window=5,min_count=5,negative=5,sample=1e-3,epoc
     model_2 = gensim.models.Word2Vec(size=300, window=window, min_count=min_count, workers=cores-1, negative=negative,sample=sample,alpha=alpha)
     model_2.build_vocab(sentences)
     total_examples = model_2.corpus_count
-    print(f'Total examples : {total_examples}')
+    # print(f'Total examples : {total_examples}')
 
-    # model_2.build_vocab([list(model.wv.vocab.keys())], update=True)
-    # model_2.intersect_word2vec_format('/home/31265/10K_Pandemic/glove.6B.300d.w2vformat.txt', binary=False, lockf=1.0)
+    model_2.build_vocab([list(model.wv.vocab.keys())], update=True)
+    model_2.intersect_word2vec_format('text_data/glove_pretrained/glove.6B.300d.w2vformat.txt', binary=False, lockf=1.0)
 
     model_2.train(sentences, total_examples=total_examples, epochs=epoch, compute_loss=True, callbacks=[cobj])
 
@@ -113,53 +113,60 @@ def search_results(text,model,search_dict):
 def main():
     ## key words to find ##
     in_path = 'text_data/'
-    out_path = 'models/'
-    if not os.path.isdir(out_path):
-        os.mkdir(out_path)
+    # out_path = 'models/'
+    # if not os.path.isdir(out_path):
+    #     os.mkdir(out_path)
     files = glob(in_path+'*.txt')
-    # params = {
-    #         2008:{'x_alpha': 0.00147651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2009:{'x_alpha': 0.00147651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2010:{'x_alpha': 0.00147651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2011:{'x_alpha': 0.00147651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2010:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2012:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2013:{'x_alpha': 0.0017651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.0064077618550642e-05, 'x_window': 2.0},
-    #         2014:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         2015:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         2016:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         # 2016:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         2017:{'x_alpha': 0.00147651091635882, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         2018:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0},
-    #         2019:{'x_alpha': 0.0018590758976614816, 'x_epoch': 100.0, 'x_min_count': 3.0, 'x_negative': 5.0, 'x_sample': 1.00e-05, 'x_window': 2.0}}
 
-    for file in [files[3]]:
-        best_params = {'x_alpha': 1e-6, 'x_epoch': 500, 'x_min_count': 1, 'x_negative': 10, 'x_sample': 0, 'x_window': 5}
-        # best_params['x_epoch'] = 500
-        print(file, best_params)
+    text = []
+    print('Getting text data')
+    for file in files:
         with open(file) as f:
-            text = list(filter(None, f.read().split('\n')))
+            raw_text = list(filter(None, f.read().split('\n')))
             f.close()
-
-        text = [[x.casefold() for x in line.split(' ')] for line in text]
-        cobj = callback()
-        test_model = model_build(text, cobj, window=int(best_params['x_window']), min_count=int(best_params['x_min_count']), negative=int(best_params['x_negative']),
-                                sample=best_params['x_sample'], epoch=int(best_params['x_epoch']), alpha=best_params['x_alpha'])
-        # test_model.wv.save_word2vec_format(out_path+file.split('\\')[-1].split(".")[0]+'.model')
-        # test_model.wv.save_word2vec_format(out_path+file.split('\\')[-1].split(".")[0]+'.txt')
-        print('-'*50)
-        print(test_model.wv.evaluate_word_pairs(datapath('wordsim353.tsv')))
-        print(test_model.wv.evaluate_word_analogies(datapath('questions-words.txt'))[0])
-        print('-'*50)
-        fig, axis = plt.subplots(1,1,figsize=(15,10))
-        axis.set_xlabel('Epochs')
-        axis.set_ylabel('Loss')
-        axis.plot(cobj.losses, color='blue')
-        plt.show()
-        break
-        del test_model
-        gc.collect()
+        int_text = [[x.casefold() for x in line.split(' ')] for line in raw_text]
+        text.extend(int_text)
+        # filename = file.split('\\')[-1].split('.')[0]
+        # outfile_path = f'tuned_params/{filename}.pkl'
+        # print(outfile_path)
+    print('Tuning parameters')
+    best_params = {
+        'text' : text,
+        'x_alpha': hp.loguniform('lr',np.log(1e-7),np.log(1e-3)), 'x_epoch': 20,
+        'x_min_count': 1, 'x_negative': hp.quniform('negative',5,20,1),
+        'x_sample': hp.loguniform('sample',np.log(1e-15),np.log(1e-5)), 'x_window': hp.quniform('window',2,5,1)
+    }
+    tuned_params = fmin(tuner, best_params, algo=tpe.suggest, max_evals=50)
+    outfile = open('tuned_params/combined_files.pkl','wb')
+    pickle.dump(tuned_params,outfile)
+    outfile.close()
+    # test_model = model_build(text, cobj, window=int(params['x_window']),
+    #                          min_count=int(params['x_min_count']), negative=int(params['x_negative']),
+    #                          sample=params['x_sample'], epoch=int(params['x_epoch']), alpha=params['x_alpha'])
+    # test_model.wv.save_word2vec_format(out_path+file.split('\\')[-1].split(".")[0]+'.model')
+    # test_model.wv.save_word2vec_format(out_path+file.split('\\')[-1].split(".")[0]+'.txt')
+    # print('-'*50)
+    # print(test_model.wv.evaluate_word_pairs(datapath('wordsim353.tsv')))
+    # print(test_model.wv.evaluate_word_analogies(datapath('questions-words.txt'))[0])
+    # print('-'*50)
+    # fig, axis = plt.subplots(1,1,figsize=(15,10))
+    # axis.set_xlabel('Epochs')
+    # axis.set_ylabel('Loss')
+    # axis.plot(cobj.losses, color='blue')
+    # plt.show()
+    # del test_model
+    # gc.collect()
     return
+
+def tuner(params):
+    cobj = callback()
+    test_model = model_build(params['text'], cobj, window=int(params['x_window']), min_count=int(params['x_min_count']), negative=int(params['x_negative']),
+                             sample=params['x_sample'], epoch=int(params['x_epoch']), alpha=params['x_alpha'])
+    pearson_corr = test_model.wv.evaluate_word_pairs(datapath('wordsim353.tsv'))[0][0]
+    del test_model
+    gc.collect()
+    print(f'Pearson correlation : {-1*pearson_corr}')
+    return -1*pearson_corr
 
 if __name__ == '__main__':
     main()
